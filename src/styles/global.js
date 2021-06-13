03ef440d3038c98e2898a74cc7c94f2e416f7c38
Sarah Kirby <sarah@neverbland.com>
2021-06-13 17:46:21 +1200

    Update global styles - spacing variables etcs

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,12 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
-import FONTS from './fonts';
+import BREAKPOINTS from './breakpoints';
 import COLORS from './colors';
+import FONTS from './fonts';
+import { GRID_WIDTH } from './grid';
 
 export default createGlobalStyle`
   :root {
     --gutter-side: 20px;
-    --max-width: 1280px;
+    --max-width: ${GRID_WIDTH}px;
+
+    --space-sm: 30px;
+    --space-lg: 80px;
 
     --color-bg: ${COLORS.beige};
     --color-text: ${COLORS.black};
@@ -18,10 +23,13 @@ export default createGlobalStyle`
     --line-height-sm: 20px;
     --line-height-lg: 32px;
 
-    @media (min-width: 768px) {
-      --gutter-side: 80px;
+    @media (min-width: ${BREAKPOINTS.small}) {
       --font-size-lg: 37px;
       --line-height-lg: 44px;
+    }
+
+    @media (min-width: ${BREAKPOINTS.medium}) {
+      --gutter-side: 80px;
     }
   }
 
@@ -52,6 +60,7 @@ export default createGlobalStyle`
   a {
     color: inherit;
     cursor: pointer;
+    text-decoration: none;
   }
 
   h1,
