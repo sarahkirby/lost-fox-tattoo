69dbf2cdbbc6d7f42d6ff4888b017c17469015d4
Sarah Kirby <sarah@neverbland.com>
2021-06-13 17:44:18 +1200

    Add constants utils
    email, insta url, google maps link

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const INSTAGRAM_URL = 'https://www.instagram.com/lostfoxtattoo/';
+export const CONTACT_EMAIL = 'hello@tashcosmos.co.uk';
+export const GOOGLE_MAPS_URL =
+  'https://www.google.com/maps/dir//258+Pentonville+Rd,+London+N1+9JY/@51.5310671,-0.1207743,19z/data=!4m17!1m7!3m6!1s0x48761b3f2987c38f:0xc7447abdb4421212!2s258+Pentonville+Rd,+London+N1+9JY!3b1!8m2!3d51.5310671!4d-0.1202271!4m8!1m0!1m5!1m1!1s0x48761b3f2987c38f:0xc7447abdb4421212!2m2!1d-0.1202271!2d51.5310671!3e1';
